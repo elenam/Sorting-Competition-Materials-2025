cb705c8c9b8db6efa62b298a189d8fce580832d1
Elena M <elenam@morris.umn.edu>
2025-10-04 12:49:05 -0500

    added standard methods, changed reading to separate out the target string

--- src/Group0.java
+++ src/Group0.java
@@ -3,6 +3,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 // To run on a single core, compile and then run as:
@@ -10,29 +12,28 @@ import java.util.Scanner;
 // To avoid file reading/writing connections to the server, run in /tmp 
 // of your lab machine.
 
-
 public class Group0 {
+	private static String target;
 
 	public static void main(String[] args) throws InterruptedException, FileNotFoundException {
 
 		if (args.length < 2) {
-			System.out.println(
-					"Please run with two command line arguments: input and output file names");
+			System.out.println("Please run with two command line arguments: input and output file names");
 			System.exit(0);
 		}
 
 		String inputFileName = args[0];
 		String outFileName = args[1];
-		
+
 		// Uncomment to test comparator methods
-		//SortingCompetitionComparator.runComparatorTests();
+		// SortingCompetitionComparator.runComparatorTests();
 
 		String[] data = readData(inputFileName); // read data as strings
-		
+
 		String[] toSort = data.clone(); // clone the data
 
 		sort(toSort); // call the sorting method once for JVM warmup
-		
+
 		toSort = data.clone(); // clone again
 
 		Thread.sleep(10); // to let other things finish before timing; adds stability of runs
@@ -48,7 +49,7 @@ public class Group0 {
 		writeOutResult(toSort, outFileName); // write out the results
 
 	}
-	
+
 	private static String[] readData(String inputFileName) throws FileNotFoundException {
 		ArrayList<String> input = new ArrayList<>();
 		Scanner in = new Scanner(new File(inputFileName));
@@ -58,21 +59,35 @@ public class Group0 {
 		}
 
 		in.close();
+		
+		// move the target string out of data into a global variable:
+		target = input.get(input.size() - 1);
+		input.remove(input.size() - 1);
 
 		// the string array is passed just so that the correct type can be created
 		return input.toArray(new String[0]);
 	}
-	
+
 	// YOUR SORTING METHOD GOES HERE.
 	// You may call other methods and use other classes.
 	// Note: you may change the return type of the method.
 	// You would need to provide your own function that prints your sorted array to
 	// a file in the exact same format that my program outputs
-	
+
 	private static void sort(String[] toSort) {
-		//Arrays.sort(toSort, new SortingCompetitionComparator());
+		Arrays.sort(toSort, new SortingCompetitionComparator());
 	}
-	
+
+	private static class SortingCompetitionComparator implements Comparator<String> {
+
+		@Override
+		public int compare(String s1, String s2) {
+
+			return 0;
+
+		}
+	}
+
 	private static void writeOutResult(String[] sorted, String outputFilename) throws FileNotFoundException {
 		PrintWriter out = new PrintWriter(outputFilename);
 		for (String s : sorted) {
